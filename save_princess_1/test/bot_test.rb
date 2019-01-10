7da1bfb493a958fd850ae0b1cae6005aea71e181
Angela Duran <duranangela19@gmail.com>
2019-01-10 12:30:41 -0700

    Refactor displayPath method, add sort to test assertions

--- save_princess_1/test/bot_test.rb
+++ save_princess_1/test/bot_test.rb
@@ -21,17 +21,17 @@ class BotTest < Minitest::Test
 
   def test_it_can_find_path_3x3_grid
     bot = Bot.new(3, ['--p', '-m-', '---'])
-    assert_equal ["UP\n", "RIGHT\n"], bot.displayPath
+    assert_equal ["UP\n", "RIGHT\n"].sort, bot.displayPath.sort
   end
 
   def test_it_can_find_path_5x5_grid
     bot = Bot.new(5, ["-----", "-----", "--m--", "-----", "p----"])
-    assert_equal ["DOWN\n", "DOWN\n", "LEFT\n", "LEFT\n"], bot.displayPath
+    assert_equal ["DOWN\n", "DOWN\n", "LEFT\n", "LEFT\n"].sort, bot.displayPath.sort
   end
 
   def test_it_can_find_path_7x7_grid
     bot = Bot.new(7, ["p------", "-------", "-------", "---m---", "-------", "-------", "-------"])
-    assert_equal ["UP\n", "UP\n", "UP\n", "LEFT\n", "LEFT\n", "LEFT\n"], bot.displayPath
+    assert_equal ["UP\n", "UP\n", "UP\n", "LEFT\n", "LEFT\n", "LEFT\n"].sort, bot.displayPath.sort
   end
 
 end
