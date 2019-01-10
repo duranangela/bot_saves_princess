8cd3cf0488630787af16b3133324a7dfc1a30053
Angela Duran <duranangela19@gmail.com>
2019-01-10 10:31:12 -0700

    Add method display_path

--- save_princess_1/lib/bot.rb
+++ save_princess_1/lib/bot.rb
@@ -18,4 +18,35 @@ class Bot
     princess
   end
 
+  def displayPath
+    moves = @n/2
+    solution = []
+    princess = find_princess
+
+    if princess[0] == 0
+      (moves).times do
+        solution << "UP\n"
+      end
+    else
+      (moves).times do
+        solution << "DOWN\n"
+      end
+    end
+
+    if princess[1] == 0
+      (moves).times do
+        solution << "LEFT\n"
+      end
+    else
+      (moves).times do
+        solution << "RIGHT\n"
+      end
+    end
+
+  solution
+  end
+
 end
+
+bot = Bot.new(5, ["----p", "-----", "--m--", "-----", "-----"])
+bot.displayPath
--- save_princess_1/test/bot_test.rb
+++ save_princess_1/test/bot_test.rb
@@ -21,7 +21,7 @@ class BotTest < Minitest::Test
 
   def test_it_can_find_path_3x3_grid
     bot = Bot.new(3, ['--p', '-m-', '---'])
-    assert_equal "UP\n, RIGHT\n", bot.displayPath
+    assert_equal ["UP\n", "RIGHT\n"], bot.displayPath
   end
 
 end
