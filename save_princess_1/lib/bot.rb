2afbedeefc5bdbc2dda37b7ebaa97f4249381b02
Angela Duran <duranangela19@gmail.com>
2019-01-10 10:17:25 -0700

    Add method find_princess

--- save_princess_1/lib/bot.rb
+++ save_princess_1/lib/bot.rb
@@ -5,4 +5,17 @@ class Bot
     @grid = grid
   end
 
+  def find_princess
+    princess = ""
+    @grid.each_with_index do |row, r_ind|
+      row.chars.each_with_index do |col, c_ind|
+        if col == "p"
+          princess = [r_ind, c_ind]
+          break
+        end
+      end
+    end
+    princess
+  end
+
 end
