05e6ca6aef797e0f267861f2d50893f660d08ee4
Angela Duran <duranangela19@gmail.com>
2019-01-10 10:19:18 -0700

    Add another test for locating princess

--- save_princess_1/test/bot_test.rb
+++ save_princess_1/test/bot_test.rb
@@ -9,9 +9,14 @@ class BotTest < Minitest::Test
     assert_instance_of Bot, bot
   end
 
-  def test_it_can_locate_the_princess
+  def test_it_can_locate_the_princess_with_3x3_grid
     bot = Bot.new(3, ['--p', '-m-', '---'])
     assert_equal [0,2], bot.find_princess
+  end
+
+  def test_it_can_locate_the_princess_with_5x5_grid
+    bot = Bot.new(5, ["-----", "-----", "--m--", "-----", "p----"])
+    assert_equal [4, 0], bot.find_princess
   end
 
 end
